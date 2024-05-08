b9e9aa3788e151ada560d2b249418a1b8d82856a
mfabianchm <mfchongm@hotmail.com>
2024-05-08 12:13:18 -0600

    Initial Commit

---
+++ NotificationCenter-ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationCenter-ExampleApp
+//
+//  Created by Marcos Fabian Chong Megchun on 08/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
